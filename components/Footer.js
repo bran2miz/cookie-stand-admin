7ace6455edf2d98d2e2f3004b8dd4989f5edbe66
brandonmizutani <bran2miz@gmail.com>
2022-04-06 20:53:58 -0700

    finished css portion of this lab

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+
+export default function Footer() {
+  return(
+  <footer className="p-3 justify-evenly bg-emerald-500">
+    <nav>
+      <h1 className='text-sm'>
+        ©2021
+      </h1>
+    </nav>
+  </footer>
+  )
+}
---
+++ components/Header.js
@@ -0,0 +1,10 @@
+
+export default function Header() {
+  return(
+    <header className='flex items-center justify-between p-3 bg-emerald-500'>
+        <h1 className="text-4xl">
+          Cookie Stand Admin
+        </h1>
+      </header>
+  )
+}
