b0c53ab89679723da55e9a3cb71a6501687080cc
brandonmizutani <bran2miz@gmail.com>
2022-04-07 19:06:52 -0700

    completed the CSS portion of the lab assignment

--- components/CreateForm.js
+++ components/CreateForm.js
@@ -1,43 +1,42 @@
 
+export default function CreateForm(props) {
+  function handleSubmit(event) {
+    event.preventDefault;
 
-export default function CreateForm() {
+    props.onCreate(event.target.location.value, Number(event.target.min_cust_per_hour.value), Number(event.target.max_cust_per_hour.value), Number(event.target.avg_cookies_per_sale.value))
 
-return(
-<form
-      className="w-11/12 px-3 pb-2 mx-auto text-sm rounded-lg bg-emerald-300"
-      
-    >
-      <h2 className="p-3 text-xl text-center">Create Cookie Stand</h2>
+  }
+  return(
+    <form onSubmit={handleSubmit} className="w-11/12 px-3 pb-2 mx-auto text-sm rounded-lg bg-emerald-300">
+      <h2 className="p-3 text-xl font-bold text-center">Create Cookie Stand</h2>
+        <div className="flex m-2">
+          <label className="pr-2">Location</label>
+          <input name="location" type="text" className="flex-grow" placeholder="Barcelona" required></input>
+        </div>
 
-      <div className="flex m-2">
-        <label className="pr-2">Location</label>
-        <input id="location" type="text" className="flex-grow" required></input>
-      </div>
-
-      <div className="flex mt-5">
-        <div className="w-1/4 p-2 mx-1 my-2 rounded bg-emerald-300">
+        <div className="flex mt-5">
+          <div className="w-1/4 p-2 mx-1 my-2 rounded bg-emerald-300">
           <label className="block mx-auto text-center">
             Minimum Customers per Hour
           </label>
-          <input id="min_cust_per_hour" type="text" className="w-full"></input>
+          <input name="min_cust_per_hour" type="text" placeholder="2" className="w-full"></input>
         </div>
         <div className="w-1/4 p-2 mx-1 my-2 rounded bg-emerald-300">
           <label className="block mx-auto text-center">
             Maximum Customers per Hour
           </label>
-          <input id="max_cust_per_hour" type="text" className="w-full"></input>
+          <input name="max_cust_per_hour" type="text" placeholder="4" className="w-full"></input>
         </div>
         <div className="w-1/4 p-2 mx-1 my-2 rounded bg-emerald-300">
           <label className="block text-center">Average Cookies per Sale</label>
-          <input
-            id="avg_cookies_per_sale"
-            type="text"
-            className="w-full"
-          ></input>
+          <input name="avg_cookies_per_sale" type="text" placeholder="2.5"
+          className="w-full">
+
+          </input>
         </div>
         <div className="flex items-center justify-center w-1/4 p-2 mx-1 my-2 bg-emerald-500">
           <button className="w-1/4 mx-1 bg-emerald-500" type="submit">
-            Create
+          Create
           </button>
         </div>
       </div>
